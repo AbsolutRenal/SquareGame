1849d18cbf134852fcb22a84c20446a4074d4a61
AbsolutRenal <absolut.renal@hotmail.fr>
2014-08-01 15:42:06 +0200

    icon + splashscreen

---
+++ TEST_CubesGame/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  TEST_CubesGame
+//
+//  Created by AbsolutRenal on 01/08/2014.
+//  Copyright (c) 2014 AbsolutRenal. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
