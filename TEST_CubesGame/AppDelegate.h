544e4250c7f643f300cb6fc6ac4c93b13cc69556
AbsolutRenal <absolut.renal@hotmail.fr>
2014-08-01 15:16:45 +0200

    Initial Commit

---
+++ TEST_CubesGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TEST_CubesGame
+//
+//  Created by AbsolutRenal on 01/08/2014.
+//  Copyright (c) 2014 AbsolutRenal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TEST_CubesGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TEST_CubesGame
+//
+//  Created by AbsolutRenal on 01/08/2014.
+//  Copyright (c) 2014 AbsolutRenal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
